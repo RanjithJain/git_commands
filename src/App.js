93df00df8a750ae869f26ed19a59ab0956ea6ce4
Ranjith K Jain <ranjithkjain@RanjithKJ-MBP.local>
2021-05-13 23:59:08 +0530

    Modified App.js file in master

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 function App() {
-  return (
-    <div>
-      Welcome Ranjith !!! This is master branch . Made changes in master branch
-      only
-    </div>
-  );
+  return <div>Welcome Ranjith !!!</div>;
 }
 
 export default App;
